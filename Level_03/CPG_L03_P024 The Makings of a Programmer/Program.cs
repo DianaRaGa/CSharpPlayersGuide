4ffd14c5916ff9af5a6b115464f698e04df2ae9b
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2026-08-29 12:26:23 -0500

    Created the 3rd project

---
+++ Level_03/CPG_L03_P024 The Makings of a Programmer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi Ritling friends");
+Console.WriteLine("I don't know where I am");
+Console.WriteLine("Or what it all means");
+Console.WriteLine("But I will be learning");
+Console.WriteLine("And keep getting better");
