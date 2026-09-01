c9ca8cea7601639ab5227d514032a484341c5911
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2026-08-31 20:58:53 -0500

    Created the folder and the new challenge for level 4

---
+++ Level_04/CPG_L04_P031/Program.cs
@@ -0,0 +1,15 @@
+﻿/* L04 The Thing Namer 3000
+ * This code is to learn how to comment on C#
+ */
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // This is the variable to store the thing
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // Variable to store the description of the thing
+string c = " of Doom 3000!";
+Console.WriteLine("The " + b + " " + a + c); // Complete Print out name for Commenton ʕっ•ᴥ•ʔっ⭐
+
+/* 
+ * What I did to make this code more understandable is to make the number or variables smaller and compact the string information into one line only.
+ * Also I belive that the bug in the text was the duplicated of 
+ */
