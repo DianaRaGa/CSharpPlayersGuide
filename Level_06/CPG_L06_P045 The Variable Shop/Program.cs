3647d48a76843c97c297ec589fa63189bfdc99cc
Diana Ramírez Gamboa <dianaramirezgamboa@outlook.com>
2026-09-04 20:57:31 -0500

    Advanced on the list of the 14 types of variables

--- Level_06/CPG_L06_P045 The Variable Shop/Program.cs
+++ Level_06/CPG_L06_P045 The Variable Shop/Program.cs
@@ -2,12 +2,36 @@
 
 // Integer Types //
 
-// 1. int 4 bytes and between -2 billion and +2 billion
-int anIntegerType = 10_000;
-Console.WriteLine("int = "+ anIntegerType);
 
-// 2. short between -32,000 and +32,000
+// 1. short between -32,000 and +32,000 (Int16)
 short aShortType = -56;
-Console.WriteLine("short" + aShortType);
+Console.WriteLine("variable = " + aShortType + " type = " + aShortType.GetType());
 
-// 3. 
+// 2. int 4 bytes and between -2 billion and +2 billion (Int32)
+int anIntegerType = 10_000;
+Console.WriteLine("variable = " + anIntegerType + " type = " + anIntegerType.GetType());
+
+// 3. long between -9 quintillion and +9 quintillion (bilioin of a billion (˶°ㅁ°)!! (Int64)
+long aLongVariable = -7_000_000_000_000_000_000;
+Console.WriteLine("variable = " + aLongVariable + " type = " + aLongVariable.GetType());
+
+// 4. ushort between 0 and 4,294,967,295 (Int16)
+uint aUShortType = 298_000_980;
+Console.WriteLine("variable = " + aUShortType + " type = " + aUShortType.GetType());
+
+// 5. uint between 0 and 65,535 (Int32)
+ushort aUIntType = 37_000;
+Console.WriteLine("variable = " + aUIntType + " type = " + aUIntType.GetType());
+
+// 6. ulong between 0 and 18 quintillion (bilioin of a billion (˶°ㅁ°)!!!!!!!! (Int64)
+ulong aULongVariable = 14_000_000_000_000_000_000;
+Console.WriteLine("variable = " + aULongVariable + " type = " + aULongVariable.GetType());
+
+// 7. sbyte between -128 and +127 (SByte)
+sbyte aSByteVariable = 15;
+Console.WriteLine("variable = " + aSByteVariable + " type = " + aSByteVariable.GetType());
+
+// 8. byte between 0 and 255 (Int16)
+byte aByteType = 78;
+Console.WriteLine("variable = " + aByteType + " type = " + aByteType.GetType());
+
